50bc0252b4b7d517e82dcdedac80be0eadcaee15
Santiago Feliu <sfeliu@dc.uba.ar>
2019-04-23 17:31:41 -0300

    Subo los cambios para procesar las dos fuentes.

--- scapyTP1.py
+++ scapyTP1.py
@@ -7,6 +7,7 @@ from scapy.all import *
 total = 0
 all_packets = []
 
+
 def MostrarNodosDistinguidos(source):
     H = 0
     N = sum(source.values())
@@ -27,26 +28,26 @@ def MostrarNodosDistinguidos(source):
 def entropy_callback(pkt):
     global total
     global all_packets
-    #try:
-    #    if pkt[ARP].op == 1:  # who-has (request)
-    #        if pkt[ARP].psrc not in wh_src: wh_src[pkt[ARP].psrc] = 0
-    #        wh_src[pkt[ARP].psrc] += 1
-    #        if pkt[ARP].pdst not in wh_dst: wh_dst[pkt[ARP].pdst] = 0
-    #        wh_dst[pkt[ARP].pdst] += 1
+    try:
+        if pkt[ARP].op == 1:  # who-has (request)
+            if pkt[ARP].psrc not in wh_src: wh_src[pkt[ARP].psrc] = 0
+            wh_src[pkt[ARP].psrc] += 1
+            if pkt[ARP].pdst not in wh_dst: wh_dst[pkt[ARP].pdst] = 0
+            wh_dst[pkt[ARP].pdst] += 1
 
-    #    if pkt[ARP].op == 2:  # is-at (response)
-    #        if pkt[ARP].psrc not in ia_src: ia_src[pkt[ARP].psrc] = 0
-    #        ia_src[pkt[ARP].psrc] += 1
-    #        if pkt[ARP].pdst not in ia_dst: ia_dst[pkt[ARP].pdst] = 0
-    #        ia_dst[pkt[ARP].pdst] += 1
-    #    total += 1
-    #except:
-    #    return
+        if pkt[ARP].op == 2:  # is-at (response)
+            if pkt[ARP].psrc not in ia_src: ia_src[pkt[ARP].psrc] = 0
+            ia_src[pkt[ARP].psrc] += 1
+            if pkt[ARP].pdst not in ia_dst: ia_dst[pkt[ARP].pdst] = 0
+            ia_dst[pkt[ARP].pdst] += 1
+        total += 1
+    except:
+        return
 
     os.system("clear")
-    pkt.show()
-    pkt.pdfdump()
-    a = input()
+    #pkt.show()
+    #pkt.pdfdump()
+    #a = input()
     #all_packets.append(pkt[ETHER])
     #print('Total amount of packets captured: ' + str(len(all_packets)))
     #if len(all_packets) > 20:
@@ -57,7 +58,7 @@ def entropy_callback(pkt):
     #            different_protocols[packet.type()] = []
     #        different_protocols[packet.type()].append(packet)
     #    exit()
-# MostrarNodosDistinguidos(wh_dst)
+    MostrarNodosDistinguidos(wh_dst)
 # MostrarNodosDistinguidos(wh_src)
 
 
